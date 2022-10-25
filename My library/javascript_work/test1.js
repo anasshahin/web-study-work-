463004829abc923c6145a3e4b3be49c5a5295070
anasshahin <90142622+anasshahin@users.noreply.github.com>
2022-10-25 14:47:29 +0300

    i

---
+++ My library/javascript_work/test1.js
@@ -0,0 +1,14 @@
+//document.write(' hala madrid      hal madrid');
+//document.querySelector('h1').style.color = 'red';
+//document.querySelector('h2').style.color = 'green';
+// make html run first and after run make js code that is inside onload
+// ctrl + / it is make comments and uncomment
+//window.alert("hello world");// text show before html window show
+//alert('hello');
+onload = function(){  
+    document.querySelector('h2').style.color = 'yellow';
+    document.querySelector('h1').style.color = 'red';
+    document.querySelector('h3').style.color = 'blue';
+    console.log("one day i hope "); // write in console
+}
+
---
+++ My library/javascript_work/test2.js
@@ -0,0 +1,24 @@
+console.log('hello');
+console.error('error');
+ window.print();
+ var name = 'anas';
+ 
+ console.log(name);
+ name = 10;
+ console.log(' my name is '+name);
+ console.log(name**2 );
+ console.log('5' /'2'); 
+ console.log(+'5' + +'2'); 
+ console.log(-'5');
+ console.log(-'-5');
+ 
+ console.log(+false ) ;
+ console.log(+true ) ;
+ console.log(Number('4') ) ;
+ console.log(+'ahm' ) ; 
+ console.log(parseInt('1 anas') ) ;
+ console.log(parseFloat('1.9 anas') ) ;
+
+ 
+ 
+ 
