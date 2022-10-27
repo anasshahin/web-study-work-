76db9ca581a7f668786e5492ee18b6b27a92052a
anasshahin <90142622+anasshahin@users.noreply.github.com>
2022-10-27 13:58:47 +0300

    i

--- My library/javascript_work/test2.js
+++ My library/javascript_work/test2.js
@@ -152,4 +152,65 @@ console.log(names);
 console.log(names.pop());
 console.log(names);
 
+let names1= ['real madrid', 'barcelona','leeds','manU','manC'];
+names1.splice(0,1);// اول فراغ بمثل رقم العنصر الي من عنده رح ابلش الحذف و بالحالة هاي فهو العنصر رقم 0
+// هنا بكتب عدد العناصر الي بدي احذفها
+console.log( names1);
+names1.push('de','re','see');
+console.log( names1);
+names1.splice(2,3);
+console.log( names1);
+names1.splice(2,3,'re','me');
+// in this setuation it is remove 3 item start from item 2 then it add the items that i put in the same place that i make th remove
+// splice (start , count , add , add ,.....)
+console.log( names1);
+names1.splice(4,0,'nee','see');
+console.log( names1)
 
+console.log(names1.slice(0,2));
+// لا تقطع الاريي انما تاخذ نسخه منها
+// slice (start element, end element)
+console.log(names1);
+console.log(names1.slice(-4));
+console.log(names1.slice(-3,-1));
+console.log(names1.lastIndexOf('nee',-1));// start search from the last index
+
+console.log(names1.reverse);// show items in reversal way
+console.log(names1.sort());// sort string in Alphabetical order 
+let nnnum=[4,5,1,5,8,9.2];
+console.log(nnnum.sort());
+
+let adde=['assdf','saf','ersj','whR'];
+let adder = ['gfj','ty','yhkf','rduth','gfdj'];
+//adde = adder + adde;
+
+//console.log(adde);// add two array but final type is string 
+
+console.log(adde.concat(adder,'gsfgtg'));// dont save the final value in adde 
+// can add array with string 
+console.log(names1.join());// convert array to string 
+console.log(names1.join(' '));
+console.log(names1.join(''));
+console.log(names1.join('#'));
+
+let xxx = 'ahmad';
+let yyy = 'AHMAD';
+let zzz = '               AHMAD seeeee           ';
+
+console.log(xxx.toUpperCase());
+console.log(yyy.toLowerCase());
+console.log(zzz);
+console.log(zzz.toLowerCase().trim());
+
+console.log(5=='5');// لا تنضر الى نوع البايانات غقط الى قيمتها
+console.log(5==='5'); // تقارن القيمة و النوع ايضا
+
+console.log(12222>22 && 7=='7'); 
+console.log(12222<22 || 7=='7'); 
+
+// && => and 
+// || => or
+
+//**************************************************************************************** */
+let age = 14;
+age>10?console.log('you can play '):console.log('you can not play ')
